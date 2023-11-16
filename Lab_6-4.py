832b34e84f2f570ba3b2b2a52f3d04883ec61f51
p24jmorris <p24jmorris@fairfieldprep.org>
2023-11-16 11:19:01 -0500

    Lab 6-4 submission (wasnt here monday to submit on time)

--- Lab_6-4.py
+++ Lab_6-4.py
@@ -10,3 +10,23 @@ Order the subjects alphabetically using a method.
 Use a method to make a copy of this list and store it in a different variable.
 Use a method to order this second list in reverse alphabetical order.
 """
+
+# stores subjects studies as variables
+subjects = ['Math', 'Science', 'English']
+
+# method to add a fourth subject you have studied to the end of the list
+subjects.append('History')
+
+# method to return the index of one of the subjects in your list
+index = subjects.index('Science')
+
+# method used to order the subjects alphebetically
+subjects.sort()
+
+# method to make a copy of this list and store it in a different variable
+subjects_copy = subjects.copy()
+
+# method to order this second list in reverse alphebetical order
+subjects_copy.sort(reverse=True)
+print(subjects)
+
